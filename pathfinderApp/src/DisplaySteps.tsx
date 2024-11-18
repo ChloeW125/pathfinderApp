d778d2c4e06204a88b675b53defcf4b2f1406c3b
Mary Tripodi <mary.tripodi@icloud.com>
2024-11-18 15:44:03 -0500

    navigating with coressponding images

--- pathfinderApp/src/DisplaySteps.tsx
+++ pathfinderApp/src/DisplaySteps.tsx
@@ -1,27 +1,52 @@
 import { useRef, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
-import Map1 from './map1.png';
-import SLC1 from './SLC1.png';
+import MC1 from './maps/MC1.png';
+import SLC1 from './maps/SLC1.png';
+import {pathDic} from './pathDic.tsx';
+
+// https://www.shecodes.io/athena/38751-how-to-import-all-images-of-a-folder-into-a-react-component
+
+// https://stackoverflow.com/questions/48560592/is-it-possible-to-import-a-group-of-images-as-an-array-create-react-app-projec
+
+// const mapImgs = require.context('../maps', true);
+// const mapList = mapImgs.keys().map(image => mapImgs(image))
+
+const searchSteps = (step: any[], goal: number) => {
+  for (let i = 0; i < step.length; i++) {
+    if (step[i][0] = goal) {
+      return step[i]
+    }
+    else if (step[i][0] > goal) {
+      console.log("error")
+      return "Error"
+    }
+
+  }
+}
+
+console.log(pathDic[1][0][2])
 
 const steps = [
-  {text: "Step 1", start: Map1, end: Map1}, 
-  {text: "Step 2", start: SLC1, end: Map1},
-  {text: "Step 3", start: Map1, end: SLC1},
-  {text: "Step 4", start: SLC1, end: SLC1}];
+  searchSteps(pathDic[4], 2), 
+  searchSteps(pathDic[2], 23),
+  //searchSteps(pathDic.4, 2),
+  //{text: "Step 4", start: SLC1, end: SLC1}
+  searchSteps(pathDic[17], 8)];
 
 class Map {
-  constructor(address: string, alt: string) {
-    this.address = address;
+  constructor(addr: string, alt: string) {
+    this.addr = addr;
     this.alt = alt;
   }
 }
 
 const maps = [
-  new Map(Map1, "Homebrew Map"),
+  new Map(MC1, "Homebrew Map"),
   new Map(SLC1, "1st Floor of SLC"),
-  new Map(Map1, "Homebrew Map"),
+  new Map(MC1, "Homebrew Map"),
   new Map(SLC1, "1st Floor of SLC")
 ];
 
@@ -30,9 +55,28 @@ const MapCanvas = (adr: string) => {
   
   const canvasRef = useRef(null);
   const imgRef = useRef(null);
+  
+  let adre
+  for (let i = 0; i < maps.length; i++) {
+    if (maps[i].addr == adr) {
+      adre = maps[i].addr
+      console.log("clear")
+    }
+    console.log(adr + " " + maps[i].addr)
+  }
 
   const draw = (ctx, frameCount, canvas) => {
-    canvas.style = "background-image: url("+adr+"); background-size: contain; background-repeat: no-repeat; vertical-align: center";
+
+    
+    /*for (let i = 0; i < maps.length; i++) {
+      if (maps[i].addr == adr) {
+        adre = maps[i].addr
+        console.log("clear")
+      }
+      console.log(adr + " " + maps[i].addr)
+    } */
+
+    canvas.style = "background-image: url("+adre+"); background-size: contain; background-repeat: no-repeat; vertical-align: center";
 
     // https://www.w3schools.com/graphics/tryit.asp?filename=trycanvas_drawimage
     //const image = <img src={adr} ref={imgRef} />
@@ -122,12 +166,12 @@ function DisplaySteps() {
             <h2>Location A to Location B</h2>
             <div className="box">
               <button onClick={() => setCurr(changeStep(steps, current, -1))} className='left'>Back</button>
-              <h3>Go to {steps[current].text}</h3>
+              <h3>{current+1}. Go to {steps[current][2].substring(0, steps[current][2].length-4)}</h3>
               <button onClick={() => setCurr(changeStep(steps, current, 1))} className='right'>Next</button>
               {StepBox(steps[current])}
             </div>
             <div className="map box">
-              {MapCanvas(maps[current].address)}
+              {MapCanvas("/src/maps/"+steps[current][2])}
               
 
             </div>
@@ -151,7 +195,7 @@ function DisplaySteps() {
           <p>{//map.address
           }</p>
           
-          {MapCanvas(map.address)
+          {MapCanvas(map.addr)
           }
         </div>
     )
@@ -176,9 +220,9 @@ function DisplaySteps() {
     
     return (
       <div className='doors'>
-        <img className="door" src={step.start} width="100px" height="auto" alt={"leave from "+step.text}/>
+        <img className="door" src={MC1} width="100px" height="auto" alt={"leave from "+step[2]}/>
         <img src="https://upload.wikimedia.org/wikipedia/commons/8/82/Arrow-v2.png" width="50px" height="auto" alt="blue arrow"/>
-        <img className="door" src={step.end} width="100px" height="auto" alt={"arrive at "+step.text}/>
+        <img className="door" src={SLC1} width="100px" height="auto" alt={"arrive at "+step[2]}/>
       </div>
     )
   }
